d1fba34c1f4b076fe3df9d72dd9babfdcdd9ce33
Nathan Naze <nanaze@gmail.com>
2013-11-30 18:51:09 -0600

    Add files for packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='pystitch',
+  packages = ['pystitch'],
+  version='0.0.1',
+  description='Python tools for cross-stiching',
+  author='Nathan Naze',
+  author_email='nanaze@gmail.com',
+  url='https://pypi.python.org/pypi/pystitch'
+)
