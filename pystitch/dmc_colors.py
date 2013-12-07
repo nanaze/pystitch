91556f15e64f2407d77ab6ea35d74abccc9f1984
Nathan Naze <nanaze@gmail.com>
2013-12-06 23:15:04 -0500

    Add functionality to build the DMCColors set.

--- pystitch/dmc_colors.py
+++ pystitch/dmc_colors.py
@@ -13,12 +13,36 @@ def _GetCsvString():
     return f.read().strip()
 
 def _CreateDmcColorFromRow(row):
-  print row
   number = int(row[0])
   name = row[1]
   hex_color = row[5]
-  red, green, blue = color.RGBColorFromHexString(hex_color)
+  red, green, blue = color.RGBTupleFromHexString(hex_color)
   return DMCColor(number, name, red, green, blue)
+
+# DMC Colors singleton
+_dmc_colors = None
+
+def _CreateDMCColors():
+  global _dmc_colors  
+  csv_data = _GetCsvString()
+  lines = csv_data.splitlines()
+
+  # Skip first line
+  lines = lines[1:]
+  reader = csv.reader(lines, delimiter='\t')
+
+  dmc_colors = set()
+  for row in reader:
+   dmc_colors.add(_CreateDmcColorFromRow(row))
+
+  return dmc_colors
+      
+def GetDMCColors():
+  global _dmc_colors
+  if not _dmc_colors:
+    _dmc_colors = frozenset(_CreateDMCColors())
+  return _dmc_colors
+  
 
 class DMCColor(color.RGBColor):
   def __init__(self, number, name, red, green, blue):
@@ -26,19 +50,13 @@ class DMCColor(color.RGBColor):
     self.name = name
     super(DMCColor, self).__init__(red, green, blue)
 
+# Simple executable functionality for debugging.
 def main():
-  csv_data = _GetCsvString()
-  lines = csv_data.splitlines()
+  for color in GetDMCColors():
+    print color
 
-  # Skip first line
-  lines = lines[1:]
+if __name__ == '__main__':
+  main()    
 
-  reader = csv.reader(lines, delimiter='\t')
 
-  dmc_colors = list()
-  for row in reader:
-    dmc_colors.append(_CreateDmcColorFromRow(row))
 
-  print dmc_colors
-
-main()
