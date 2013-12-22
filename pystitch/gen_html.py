d84e1779947b2b6971d03142eca8e67c709a7b35
Nathan Naze <nanaze@gmail.com>
2013-12-22 17:59:31 -0600

    Updates to gen_html to fix dependencies, get command-line working.

--- pystitch/gen_html.py
+++ pystitch/gen_html.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+
+"""Utilities to generate HTML from an image.
+
+Can be called from commandline. Usage:
+
+gen_html.py image.png
+"""
+
 from PIL import Image
 
 import os
-
+import sys
 import image, dmc_colors
-import megaman
 
 def PrintPixelArrayTable(pix_arr):
 
@@ -24,4 +32,20 @@ def PrintPixelArrayTable(pix_arr):
     print '</td>'
   print '</table>'
 
+def _GetImage(filename):
+  main_dir = os.path.dirname(__file__)
+  megaman_path = os.path.join(main_dir, filename)
+  img = Image.open(filename).convert('RGBA')
+  return img
 
+def main(argv):
+  if len(argv) != 2:
+    sys.exit(__doc__)
+
+  path = argv[1]
+  img = _GetImage(path)
+  pix_arr = image.GetPixelArrayFromImage(img)
+  PrintPixelArrayTable(pix_arr)
+
+if __name__ == '__main__':
+  main(sys.argv)
